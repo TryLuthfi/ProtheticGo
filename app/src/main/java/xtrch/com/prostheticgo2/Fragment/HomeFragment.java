1d3908bf8c1d3c9d5cf4c6ebefc0f84c7922045a
Al Ghifari <falyoseblings@gmail.com>
2020-09-09 17:08:40 +0700

    Home icons, reload, layanan

--- app/src/main/java/xtrch/com/prostheticgo2/Fragment/HomeFragment.java
+++ app/src/main/java/xtrch/com/prostheticgo2/Fragment/HomeFragment.java
@@ -5,22 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import xtrch.com.prostheticgo2.R;
 
 public class HomeFragment extends Fragment {
-
-
     public HomeFragment() {
         // Required empty public constructor
     }
+
+    SwipeRefreshLayout reload;
+    CircleImageView menuRehab, menuPsiko, menuBelanja;
+    CardView menuLayanan;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        final View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //FindView
+        setFindView(view);
+
+        //Reload Page
+        reload.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                reload.setRefreshing(false);
+            }
+        });
 
         return view;
+    }
+
+    private void setFindView(View view){
+        reload = view.findViewById(R.id.home_fragment_reload);
+        menuRehab = view.findViewById(R.id.home_menu_rehab);
+        menuPsiko = view.findViewById(R.id.home_menu_psiko);
+        menuLayanan = view.findViewById(R.id.home_menu_layanan);
     }
 }
