c78286ebfcc96511851dbf4c9e58295f34bb2395
Al Ghifari <falyoseblings@gmail.com>
2020-09-18 01:14:38 +0700

    Upper Extrimity Info

--- app/src/main/java/xtrch/com/prostheticgo2/Fragment/UpperExFragment.java
+++ app/src/main/java/xtrch/com/prostheticgo2/Fragment/UpperExFragment.java
@@ -1,5 +1,7 @@
 package xtrch.com.prostheticgo2.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -8,17 +10,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import xtrch.com.prostheticgo2.R;
 
 public class UpperExFragment extends Fragment {
     public UpperExFragment() {}
 
+    ImageView btnInfo;
+
+    AlertDialog.Builder dialogInfo;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upper_ex, container, false);
+        View view = inflater.inflate(R.layout.fragment_upper_ex, container, false);
+
+        //FindView
+        setFindView(view);
+        //OnClick
+        setOnClick();
+
+        return view;
+    }
+
+    private void setFindView(View view){
+        btnInfo = view.findViewById(R.id.upper_ex_info_icon);
+    }
+
+    private void setOnClick(){
+        //Info
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDialogInfo();
+            }
+        });
+    }
+
+    private void setDialogInfo(){
+        dialogInfo = new AlertDialog.Builder(getContext());
+        dialogInfo.setTitle("Upper Extrimity");
+        dialogInfo.setMessage("Fungsi dari ekstremitas atas lebih sulit digantikan dibandingkan ekstremitas bawah. Selain untuk aktivitas motorik, ekstremitas atas juga digunakan untuk keperluan sehari-hari seperti merawat diri, interaksi dengan orang lain, dan ekspresi diri.");
+        dialogInfo.setCancelable(true);
+        dialogInfo.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialogInfo.show();
     }
 
     public interface OnFragmentInteractionListener {
