d5e4f005916c346980c1ded20151add10e56d3d0
Al Ghifari <falyoseblings@gmail.com>
2020-09-19 15:33:46 +0700

    ...

---
+++ app/src/main/java/xtrch/com/prostheticgo2/Activity/AddInformasi.java
@@ -0,0 +1,16 @@
+package xtrch.com.prostheticgo2.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import xtrch.com.prostheticgo2.R;
+
+public class AddInformasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_informasi);
+    }
+}
