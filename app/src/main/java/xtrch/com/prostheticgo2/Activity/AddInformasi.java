e6e2e5d20e2f54d1c436cb6edec6633b8bdd69d5
Al Ghifari <falyoseblings@gmail.com>
2020-09-21 17:55:40 +0700

    Add/Edit Informasi Activity

--- app/src/main/java/xtrch/com/prostheticgo2/Activity/AddInformasi.java
+++ app/src/main/java/xtrch/com/prostheticgo2/Activity/AddInformasi.java
@@ -2,15 +2,56 @@ package xtrch.com.prostheticgo2.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import xtrch.com.prostheticgo2.R;
 
 public class AddInformasi extends AppCompatActivity {
 
+    ImageView btnTambahFoto;
+    EditText etJudul, etIsi;
+    Spinner sJenis;
+    ImageButton btnBack;
+    TextView tvTitle;
+    Button btnSimpan;
+
+    String idInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_informasi);
+
+        //FindView
+        setFindView();
+        //getIntent
+        getIntentState();
+    }
+
+    private void setFindView(){
+        btnTambahFoto = findViewById(R.id.add_info_foto);
+        btnBack = findViewById(R.id.back_from_add_info);
+        etJudul = findViewById(R.id.add_info_judul);
+        etIsi = findViewById(R.id.add_info_isi);
+        sJenis = findViewById(R.id.add_info_jenis);
+        tvTitle = findViewById(R.id.add_info_title);
+        btnSimpan = findViewById(R.id.add_info_simpan);
+    }
+
+    private void getIntentState(){
+        Intent id = getIntent();
+        idInfo = id.getStringExtra("id_info");
+        if (idInfo.equals("0")){
+            tvTitle.setText("Tambah Informasi");
+        } else {
+            tvTitle.setText("Edit Informasi");
+        }
     }
 }
