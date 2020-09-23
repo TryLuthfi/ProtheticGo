f6b00207c10ff4ce1943baeda079d867ae690db7
Al Ghifari <falyoseblings@gmail.com>
2020-09-23 22:35:56 +0700

    Psiko to Add Psiko

--- app/src/main/java/xtrch/com/prostheticgo2/Activity/Psikologi.java
+++ app/src/main/java/xtrch/com/prostheticgo2/Activity/Psikologi.java
@@ -2,15 +2,18 @@ package xtrch.com.prostheticgo2.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import xtrch.com.prostheticgo2.R;
 
 public class Psikologi extends AppCompatActivity {
 
     ImageButton mBack;
+    ImageView btnAddPsiko;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,16 @@ public class Psikologi extends AppCompatActivity {
 
         //FindView
         setFindView();
+        //OnClick
+        setOnClick();
+    }
+
+    private void setFindView(){
+        mBack = findViewById(R.id.back_from_psiko);
+        btnAddPsiko = findViewById(R.id.psiko_add_btn);
+    }
+
+    private void setOnClick(){
         //Back
         mBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +39,13 @@ public class Psikologi extends AppCompatActivity {
                 finish();
             }
         });
-    }
-
-    private void setFindView(){
-        mBack = findViewById(R.id.back_from_psiko);
+        btnAddPsiko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addPsiko = new Intent(Psikologi.this, AddPsikologi.class);
+                addPsiko.putExtra("id_psiko", "0");
+                startActivity(addPsiko);
+            }
+        });
     }
 }
